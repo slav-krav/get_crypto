84e1e2586b1adeaf8b9e30e08c9a5c451abd332c
Viacheslav Kravchenko <Viacheslav_Kravchenko@epam.com>
2024-03-29 18:29:09 +0300

    Implemented binance parser + 1 smoke test parametrization

---
+++ src/data_gatherers/parsers/binance.py
@@ -0,0 +1,29 @@
+import logging
+
+import pydantic
+
+from data_gatherers.parsers.abstract import AbstractParser
+from models import AllPrices, Price
+
+
+class BinanceParser(AbstractParser):
+    def parse_response(self, data: dict or None) -> AllPrices:
+        if data is None:
+            return AllPrices.get_empty()
+
+        try:
+            all_prices = {}
+            for price in data:
+                price = self.parse_price(price)
+                if price is not None:
+                    all_prices[price.symbol] = price
+            return AllPrices(prices=all_prices)
+        except KeyError:
+            logging.error(f'{self.__class__.__name__} is unable to parse data. Wrong format? Raw data:\n{data}')
+            return AllPrices.get_empty()
+
+    def parse_price(self, item: dict) -> Price or None:
+        try:
+            return Price(symbol=item['symbol'], price=item['price'])
+        except (KeyError, pydantic.ValidationError):
+            logging.error(f'Unable to construct Price for item {item}')
--- tests/test_data_gatherers/test_gatherer.py
+++ tests/test_data_gatherers/test_gatherer.py
@@ -1,12 +1,22 @@
+from typing import Type
+
+import pytest
+
 from data_gatherers.gatherer import DataGatherer
+from data_gatherers.parsers.abstract import AbstractParser
+from data_gatherers.parsers.binance import BinanceParser
 from data_gatherers.parsers.bybit import BybitParser
 from models import AllPrices
 
 
-async def test_bybit_fetch():
+@pytest.mark.parametrize('parser_type, url', [
+    [BybitParser, 'https://api.bybit.com/v2/public/tickers'],
+    [BinanceParser, 'https://api.binance.com/api/v3/ticker/price']
+])
+async def test_bybit_fetch(parser_type: Type[AbstractParser], url: str):
     """smoke test to fetch and see if type is OK and data is there"""
-    bybit_parser = BybitParser()
-    gatherer = DataGatherer(url='https://api.bybit.com/v2/public/tickers', parser=bybit_parser)
+    parser = parser_type()
+    gatherer = DataGatherer(url=url, parser=parser)
     all_prices = await gatherer.fetch_prices()
     assert isinstance(all_prices, AllPrices)  # pydantic did validation
     assert all_prices.prices  # not empty
